04f34d303d81379b020a96183ac95497a560ca58
tslilpress <tslilpress@gmail.com>
2020-10-07 19:36:58 -0400

    Created map in render to show order
    - created a loop to run through all order object and render each.
    - work in progress, still not showing on page.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -7,7 +7,8 @@ class Order extends Component {
     super(props)
     this.state = {
       order: [],
-      isInOrder: false
+      isInOrder: false,
+      isLoaded: false
     }
   }
 
@@ -15,18 +16,36 @@ class Order extends Component {
     const user = this.props.user
     showOrder(user)
       .then(response => {
-        console.log('response is', response.data.orders[0])
+        console.log('response is', response.data)
         this.setState({
           order: response.data.orders,
-          isInOrder: true
+          isInOrder: true,
+          isLoaded: true
         })
       })
       .catch(console.error)
   }
   render () {
+    console.log('orders is', this.state.order[7])
+    const { orders } = this.state
+    let jsx
+    if (this.state.isLoaded === false) {
+      jsx = <p>Loading...</p>
+    } else if (this.state.order.length === 0) {
+      jsx = <p>You have no pending orders at this moment</p>
+    } else {
+      jsx = (
+        <ul>
+          {this.state.order.map((order) => {
+            return <li key={order._id}>{ orders }</li>
+          })}
+        </ul>
+      )
+    }
     return (
       <div>
         <h2>Current Order</h2>
+        {jsx}
       </div>
     )
   }
