24710cf3176328ec93a8405cbf592120838df18a
tslilpress <tslilpress@gmail.com>
2020-10-09 16:12:16 -0400

    Playing around with style

--- src/Flower.js
+++ src/Flower.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
 import apiUrl from './apiConfig'
 import messages from './components/AutoDismissAlert/messages'
 // import flowers from './data/flowers'
@@ -57,19 +60,19 @@ class Flower extends Component {
     let jsx
     if (this.props.user !== null) {
       jsx =
-          <div>
-            <Card border='primary' style={{ width: '18rem' }} >
-              <Card.Img variant="top" src={this.props.image} />
-              <Card.Body>
-                <Card.Title>{this.props.name}</Card.Title>
-                <Card.Text>
-                  {this.props.description}
-                  Price: {this.props.price}
-                </Card.Text>
-                <Button onClick={this.handleSubmit} variant="primary">Add to cart</Button>
-              </Card.Body>
-            </Card>
-          </div>
+      <div className='card'>
+        <Card border='primary' style={{ width: '18rem' }} >
+          <Card.Img variant="top" src={this.props.image} />
+          <Card.Body>
+            <Card.Title>{this.props.name}</Card.Title>
+            <Card.Text>
+              {this.props.description}
+                      Price: {this.props.price}
+            </Card.Text>
+            <Button onClick={this.handleSubmit} variant="primary">Add to cart</Button>
+          </Card.Body>
+        </Card>
+      </div>
     } if (this.props.user === null) {
       jsx =
       <div>
